eed5b1e73ae66920a6c5c87663b408c572bd3cea
Eduard Kutuev <ed@napych.com>
2020-10-20 12:53:00 +0300

    Initial commit

---
+++ main.php
@@ -0,0 +1,3 @@
+<?php
+
+\Difra\Bootstrap\Plugin::enable();
---
+++ src/Plugin.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Difra\Bootstrap4;
+
+/**
+ * Class Plugin
+ * @package Difra\Plugins\Bootstrap4
+ */
+class Plugin extends \Difra\Plugin
+{
+    /**
+     * @inheritdoc
+     */
+    protected function init()
+    {
+    }
+}
+
