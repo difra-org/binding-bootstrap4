5c3415aa8fe4f37f4be464f36d0e42867a2908e2
Eduard Kutuev <ed@napych.com>
2020-12-09 23:16:46 +0300

    Use View\HTML to inject scripts and styles

--- src/Plugin.php
+++ src/Plugin.php
@@ -2,6 +2,10 @@
 
 namespace Difra\Bootstrap4;
 
+use Difra\Controller;
+use Difra\Envi\Action;
+use Difra\Events\Event;
+
 /**
  * Class Plugin
  * @package Difra\Plugins\Bootstrap4
@@ -13,6 +17,14 @@ class Plugin extends \Difra\Plugin
      */
     protected function init()
     {
+        \Difra\Events\System::getInstance(Event::EVENT_RENDER_INIT)->registerHandler([static::class, 'addHTML']);
+    }
+
+    public static function addHTML(Event $event)
+    {
+        $html = Action::getController()->html;
+        $html->getHead()->addStylesheet('/bootstrap4/css/bootstrap.css');
+        $html->getBody()->addScript('/bootstrap4/js/bootstrap.js');
     }
 }
 
--- xslt/bootstrap4/bootstrap-headers.xsl
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
-    <xsl:template name="bootstrap4">
-        <link rel="stylesheet" href="/bootstrap4/css/bootstrap.css"/>
-        <script src="/bootstrap4/js/bootstrap.js"/>
-    </xsl:template>
-</xsl:stylesheet>
